94ce6ee8417f8e62e3da385e5a30dd51be090ec7
Tatiana Ryzhkova <tatvasilna@gmail.com>
2016-04-16 12:12:08 -0700

    JCommander using List

--- Title_Validation_CLO/src/main/java/core/App.java
+++ Title_Validation_CLO/src/main/java/core/App.java
@@ -1,47 +1,51 @@
 package core;
 
-import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import java.util.List;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+
+@Parameters(separators = "=")
 
 public class App {
+	public static final String LIST = "--list";
+	public static final String sLIST = "-l";
+	@Parameter(names = { LIST, sLIST }, arity = 3, description = "List")
+	private static List<String> list;
+	
+//     @Parameter(names = { "-n", "--name" }, description = "Your name", required = true)
+	@Parameter(names = { "-n", "--name" }, description = "Your name")
+	private static String name = "Bill";
+	
+	@Parameter(names = { "-a", "--age", }, description = "Your age")
+	private static int age = 18;
+	
+	@Parameter(names = { "-m", "--married" }, description = "Your marriage status")
+	private static boolean married = false;
+	
+	@Parameter(names = "--help", help = true, hidden = true)
+	private static boolean help = false;
+	
 	public static void main(String[] args) {
-
-		if (args.length == 0) {
-			System.err.println("No arguments!");
-			System.exit(1);
-		}
-
-		WebDriver driver = new FirefoxDriver();
 		
-		for (int i = 0; i < args.length; i++) {
+	new JCommander(new App(), args);
+//            JCommander cli = new JCommander(new App(), args);
+	if (help) {
+		new JCommander(new App(), args).usage();
+//                  cli.usage();
+		System.exit(0);
+		}
+	printInfo();
 
-			String text_case_id = "TC-001.0" + (i + 1);
-			String param[] = args[i].split("\\|");
-
-			String url = param[0];
-			String title_expected = param[1];
-			
-			driver.get(url);
-			driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-			String title_actual = driver.getTitle();
-				System.out.println("");
-			if (title_expected.equals(title_actual)) {
-				System.out.println("Test Case ID: \t\t" + text_case_id);
-				System.out.println("URL: \t\t\t" + url);
-				System.out.println("Title Expected: \t" + title_expected);
-				System.out.println("Title Actual: \t\t" + title_actual);
-				System.out.println("Test Case Result: \t" + "PASSED");
-			} else {
-				System.out.println("Test Case ID: \t\t" + text_case_id);
-				System.out.println("URL: \t\t\t" + url);
-				System.out.println("Title Expected: \t" + title_expected);
-				System.out.println("Title Actual: \t\t" + title_actual);
-				System.out.println("Test Case Result: \t" + "FAILED");
-			}
-			
 		}
 	
-		driver.quit();
+	public static void printInfo() {
+		System.out.println("Your name is " + name + ", your age is " + age + " and you are " + (married ? "married :-(" : "not married :-)"));
+//		System.out.println(list.toString());
+//		System.out.println(list.size());
+//		System.out.println(list.get(0));
+//		System.out.println(list.get(1));
+//		System.out.println(list.get(2));
 	}
 }
